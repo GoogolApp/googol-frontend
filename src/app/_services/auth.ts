674d74446494412e4d35af111fde2bec8cc84b8a
matheusps <matheusps@icloud.com>
2018-05-14 21:02:14 -0300

    correções lint, correções de imagens, configurações ios

--- src/app/_services/auth.ts
+++ src/app/_services/auth.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map'
 
 import { appConfig } from '../app.config';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#ffffff');
       this.splashScreen.hide();
     });
   }
