1781805f1d0f12e143c576345c76d85d5454d559
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-05-29 01:08:12 -0300

    Using correct route to search user

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { User } from '../../_models/user';
 import { UsersService } from '../../_services/users';
-import { EventsService } from '../../_services/events';
-import { ProfilePage } from '../profile/profile';
 import { SearchedProfilePage } from '../searched-profile/searched-profile';
 
 @Component({
@@ -13,14 +11,15 @@ import { SearchedProfilePage } from '../searched-profile/searched-profile';
 export class SearchPage implements OnInit{
 
   async ngOnInit() {
-    await this.fetchUsers();
+    //await this.fetchUsers();
   }
   constructor(public navCtrl: NavController, private userService: UsersService, private loadingController: LoadingController) {
   }
 
   private users:User[] = [];
-  public filteredUsers:User[] = [];
   public anyResult:boolean;
+  showList: boolean = false;
+  currentSearch: string;
 
   loading = this.loadingController.create({
     content: 'Buscando usuário...',
@@ -32,7 +31,7 @@ export class SearchPage implements OnInit{
     this.navCtrl.push(SearchedProfilePage);
   }
 
-  searchUser(input:string){
+  /*searchUser(input:string){
     this.filteredUsers = [];
     this.anyResult = false;
     
@@ -42,25 +41,38 @@ export class SearchPage implements OnInit{
         this.anyResult = true;
       }
     });
-  }
+  }*/
 
-  fetchUsers() {
+  async searchUser(username:string) {
+    if(this.currentSearch !== username.trim()) {
+      this.currentSearch = username;
+      if(username !== undefined && username.trim() != "") {
+        await this.fetchUsers(username);
+      } else {
+        this.showList = false;
+      }
+    }
+    //já nao faz fetch se for vazia
+    //quero que nao faca se for igual ao que ja tem
+  }
+  
+  fetchUsers(input:string) {
     this.loading.present();
 
-    this.userService.getAll().subscribe(
+    this.userService.getByUsername(input).subscribe(
       users => {
-        this.resetFields();
+        this.users = [];
         this.users = users;
+        console.log(this.users);
+        this.showList = this.users.length !== 0;
+        console.log(this.showList);
         this.loading.dismiss();
       },
       error => {
         this.loading.dismiss();
-        console.log(error)
+        console.log(error);
       }
     );
   }
 
-  private resetFields(){
-    this.users = [];
-  }
 }
