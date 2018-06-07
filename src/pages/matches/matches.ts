b2154c3197cacfa774fec37fa5445c25967bcb81
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-06 23:08:59 -0300

    Adjust filter

--- src/pages/matches/matches.ts
+++ src/pages/matches/matches.ts
@@ -29,6 +29,16 @@ export class MatchesPage implements OnInit{
     this.matchesService.getAll().subscribe(
       matches => {
         this.matches = [];
+        this.matches = matches.filter(function (match) {
+          const currDate = new Date();
+          const date = new Date(match.matchDate);
+          let time = Math.abs(date.getTime() - currDate.getTime());
+          console.log(time/3600000)
+          if(time/3600000 <= 2.5) {
+            console.log(match.homeTeam, match.awayTeam)
+            return match;
+          }
+        });
         this.matches = matches.map(function (match) {
          const date = new Date(match.matchDate);
          date.setTime(date.getTime() + date.getTimezoneOffset()*60*1000);
