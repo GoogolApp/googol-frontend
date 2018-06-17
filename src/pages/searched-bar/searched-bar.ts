5327f732c2e785b34cec9b8d1baab1920242a6cc
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-17 12:49:55 -0300

    Created searched bar page

---
+++ src/pages/searched-bar/searched-bar.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-searched-bar',
+  templateUrl: 'searched-bar.html',
+})
+export class SearchedBarPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+
+}
