79f5e7184e8dab035ee471b41347345d7995e22f
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-17 21:47:37 -0300

    Fix imports, merge ok.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {} from '@types/googlemaps';
 
 import { SignInPage } from '../pages/Public/signin/signin';
 
