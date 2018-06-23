1038c8a6a7e8595e53a4ddfec4adb5ba2f29da76
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-23 00:56:13 -0300

    Dismissing modal

--- src/pages/User/matches/filter-matches/filter-matches.ts
+++ src/pages/User/matches/filter-matches/filter-matches.ts
@@ -1,9 +1,15 @@
 import { Component } from "@angular/core";
+import { NavController, ViewController, NavParams } from "ionic-angular/";
 
 @Component({
     selector: 'filter-matches',
     templateUrl: 'filter-matches.html'
 })
 export class FilterMatchesModal {
+    constructor(public navCtrl: NavController, public viewCtrl: ViewController, public navParams: NavParams) {
+    }
     
+    dismiss() {
+        this.viewCtrl.dismiss();
+    }
 }
