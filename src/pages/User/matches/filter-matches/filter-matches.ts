48f3908adad347475ed0a435bb7b381c50024c78
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-24 19:00:18 -0300

    Basic filter of leagues

--- src/pages/User/matches/filter-matches/filter-matches.ts
+++ src/pages/User/matches/filter-matches/filter-matches.ts
@@ -1,16 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavController, ViewController, NavParams } from "ionic-angular/";
+import { Match } from "../../../../_models/match";
 
 @Component({
     selector: 'filter-matches',
     templateUrl: 'filter-matches.html'
 })
-export class FilterMatchesModal {
+export class FilterMatchesModal implements OnInit{
     constructor(public navCtrl: NavController, public viewCtrl: ViewController, public navParams: NavParams) {
     }
     
+    leagues: string[] = [];
+    selectedLeagues: string[] = [];
+    
+    ngOnInit(): void {
+        let matches:Match[] = this.navParams.get('matches');
+
+        this.leagues = matches.map(function(match) {
+            return match.league;
+        });
+        this.leagues = Array.from(new Set(this.leagues)).sort();
+    }
+
     dismiss() {
-        this.viewCtrl.dismiss();
+        this.viewCtrl.dismiss(this.selectedLeagues);
     }
 
     logIn(mail, password) {
