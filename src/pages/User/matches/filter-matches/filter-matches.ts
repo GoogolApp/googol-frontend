7cc9e50a3ee984ebbd2f87ac4cb46c842afc428c
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-26 22:06:46 -0300

    Passing favTeams info

--- src/pages/User/matches/filter-matches/filter-matches.ts
+++ src/pages/User/matches/filter-matches/filter-matches.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController, ViewController, NavParams } from "ionic-angular/";
+
 import { Match } from "../../../../_models/match";
+import { UsersService } from "../../../../_services/users";
 
 @Component({
     selector: 'filter-matches',
     templateUrl: 'filter-matches.html'
 })
 export class FilterMatchesModal implements OnInit{
-    constructor(public navCtrl: NavController, public viewCtrl: ViewController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public viewCtrl: ViewController, public navParams: NavParams, private userService: UsersService) {
     }
     
     leagues: string[] = [];
@@ -16,7 +18,7 @@ export class FilterMatchesModal implements OnInit{
     teams: string[] = [];
     selectedTeams: string[] = [];
 
-    favorites:boolean = false;
+    favorites:any = false;
     
     ngOnInit(): void {
         let matches:Match[] = this.navParams.get('matches');
@@ -53,6 +55,18 @@ export class FilterMatchesModal implements OnInit{
             'teams': this.selectedTeams,
             'favorites': this.favorites
         }
+
+        if(this.favorites) {
+            let userId = JSON.parse(localStorage.getItem('authUser')).userId;
+            this.userService.getOne(userId).subscribe(
+                async user => {
+                    filters.favorites = await user.favTeams;
+                }
+            );
+        } else {
+            filters.favorites = this.favorites;
+        }
+
         this.viewCtrl.dismiss(filters);
     }
 
