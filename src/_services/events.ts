03fd5eeb04ed779d5cdad152d2bd9553af5855f4
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-06-29 22:40:14 -0300

    Initial config service events

--- src/_services/events.ts
+++ src/_services/events.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Match } from '../_models/match';
 import { Event } from '../_models/event';
+import { appConfig } from '../app/app.config';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class EventsService {
 
+    private url: string = appConfig.apiUrl + '/matches';
+    private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    
     fakeStorage = new Array<Event>();
     fakeId = 1;
     fakeLocation = "Bar da putaria - Campina Grande";
     
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
-    getAll() {
-        return this.fakeStorage;
+    getAll() : Observable<Event[]> {
+        return this.http.get<Event[]>(this.url, this.httpOptions)
+            .map(events => {
+                return events;
+            });
     }
 
     getById(_id: string) {
