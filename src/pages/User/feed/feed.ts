da56860fe152cb3d0ea9cc736316e261db28c753
Matheus P. Silva <matheus.ps@icloud.com>
2018-08-06 22:59:46 -0300

    added feed of promotions and events, changed promotion cards, filter of current events in feed

--- src/pages/User/feed/feed.ts
+++ src/pages/User/feed/feed.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsersService } from '../../../_services/users';
 
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html'
 })
-export class FeedPage {
+export class FeedPage{
+  
+  feed = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private userService: UsersService
+  ) {
+    this.fetchFeed();
+  }
 
+  fetchFeed(){
+    let authUserId = JSON.parse(localStorage.getItem('authUser')).userId;
+    this.userService.getFeed(authUserId).then( feed => this.feed = this.fisherYatesShuffle([...feed]));
+  }
+
+  fisherYatesShuffle(arr) {
+    let currentI = arr.length, randomI;
+    while (currentI !== 0) {
+      randomI = Math.floor(Math.random() * currentI--);
+      [arr[currentI], arr[randomI]] = [arr[randomI], arr[currentI]]
+    }
+    return arr;
   }
 
 }
