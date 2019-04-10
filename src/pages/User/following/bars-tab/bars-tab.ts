841cec2d0d6aa0e85f0b6ca2a7bb5a6ef96326c3
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2019-04-10 18:38:22 -0300

    Adiciona tab de bares seguidos

--- src/pages/User/following/bars-tab/bars-tab.ts
+++ src/pages/User/following/bars-tab/bars-tab.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { NavParams, LoadingController } from "ionic-angular";
+import { Bar } from "../../../../_models/bar";
+import { UsersService } from '../../../../_services/users';
 
 @Component({
     selector: 'bars-tab',
@@ -7,7 +10,32 @@ import { Component } from "@angular/core";
 
 export class BarsTab {
 
-    constructor(){
-        
+    private bars: Bar[] = [];
+    private showPageBar: Function;
+
+    constructor(public navParams: NavParams, private userService: UsersService, private loadingController: LoadingController){
+        this.showPageBar = navParams.data.showPageBarCb;
+    }
+
+    async fetchBars() {
+        const loading = this.loading();
+        const bars = await this.userService.getFollowingBars().toPromise();
+        this.bars = bars;
+        console.log(this.bars)
+        loading.dismiss();
+    }
+
+    loading(){
+        let loading = this.loadingController.create({
+          content: 'Por favor, aguarde...',
+          spinner: 'bubbles'
+        });
+    
+        loading.present();
+        return loading;
+    }
+
+    ionViewWillEnter() {
+        this.fetchBars();
     }
 }
--- src/pages/User/following/following.ts
+++ src/pages/User/following/following.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams, LoadingController, Events, Tab } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { User } from '../../../_models/user';
-import { UsersService } from '../../../_services/users';
 import { SearchedProfilePage } from '../searched-profile/searched-profile';
+import { SearchedBarPage } from "../searched-bar/searched-bar";
 
 import { UsersTab } from "./users-tab/users-tab";
 import { BarsTab } from "./bars-tab/bars-tab";
@@ -24,6 +24,10 @@ export class FollowingPage implements OnInit{
     this.allUsersParam = {
       showPageUserCb: this.showPageUser.bind(this)
     };
+
+    this.allBarsParam = {
+      showPageBarCb: this.showPageBar.bind(this)
+    };
   }
   
   constructor(
@@ -34,6 +38,11 @@ export class FollowingPage implements OnInit{
   showPageUser(id: string) {
     localStorage.setItem('searchedUser', JSON.stringify(id));
     this.navCtrl.push(SearchedProfilePage, { "parentPage": this });
+  }
+
+  showPageBar(id: string) {
+    localStorage.setItem('searchedBar', JSON.stringify(id));
+    this.navCtrl.parent.push(SearchedBarPage);
   }
 
 }
